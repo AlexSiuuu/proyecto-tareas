c935bd09b22727ba9d223704309647ba699b4812
AlexSiuuu <alextron45@gmail.com>
2023-10-18 15:14:31 -0500

    agregando ragistros

--- front-react/src/routes/Login.jsx
+++ front-react/src/routes/Login.jsx
@@ -1,33 +1,65 @@
-const Login = () => {
+import { Component } from "react"
+import axios from "axios"
+
+const url = "http://localhost:3000/usuarios"
+
+class Login extends Component {
+
+    state = {
+        form: {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleChange = async e => {
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    iniciarSesion = async () => {
+        await axios.get(url, {params: {nombreUsuario: this.state.form.username, password: this.state.form.password}})
+        .then(response => {
+            console.log(response.data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    render () {
     return <div>
         <div className="img">
 
-<div className="contenedor">
+            <div className="contenedor">
 
-    <div className="formulario">
+                <div className="formulario">
 
-        <form>
-        
-            <h1 className='titulo-login'>Entrar</h1><br /><br />
-                            
-            <label className='nombre-usuario'>Nombre de Usuario:</label><br />
-            <input className='campo-usuario' type="text"/><br /><br />
-                            
-            <label className='nombre-usuario'>Ingrese Contraseña:</label><br />
-            <input className='campo-usuario' type="password"/><br /><br />
+                    <form name="form">
+                    
+                        <h1 className='titulo-login'>Entrar</h1><br /><br />
+                                        
+                        <label className='nombre-usuario'>Nombre de Usuario:</label><br />
+                        <input onChange={this.handleChange} className='campo-usuario' name="username" type="text"/><br /><br />
+                                        
+                        <label className='nombre-usuario'>Ingrese Contraseña:</label><br />
+                        <input onChange={this.handleChange} className='campo-usuario' name="password" type="password"/><br /><br />
 
-            <center><button className='boton-login'>Ingresar</button></center>
+                        <center><button onClick={()=> this.iniciarSesion()} className='boton-login'>Ingresar</button></center>
 
-        </form>
-    </div>
+                    </form>
+                </div>
 
-</div>
+            </div>
 
-
-
-</div>
+        </div>
 
     </div>;
+}
 }
 
 export default Login
--- front-react/src/routes/Signup.jsx
+++ front-react/src/routes/Signup.jsx
@@ -1,41 +1,77 @@
-const Signup = () => {
-    return <div>
+import axios from "axios"
+import { Component } from "react";
+
+const url = "http://localhost:3000/usuarios"
+
+class Signup extends Component {
+
+    state = {
+        data:[],
+        form:{
+            nombre:"",
+            usuario:"",
+            correo:"",
+            password:""
+        }
+    }
+
+    peticionPost = async () => {
+        // eslint-disable-next-line no-unused-vars
+        await axios.post(url,this.state.form).then(response => {
+
+        }).catch(err => {
+            console.log(err.message)
+        })
+    }
+
+    handleChange = async e => {
+        e.persist()
+        await this.setState({
+            form:{
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
+        })
+        console.log(this.state.form)
+    }
+
+    render () {
+        const {form} = this.state
+    return (<div>
         <div className="img">
 
-<div className="contenedor">
+            <div className="contenedor">
 
-    <div className="formulario-registro">
+                <div className="formulario-registro">
 
-        <form name="form">
+                    <form name="form">
         
-            <h1 className='titulo-login'>Registrate</h1><br />
+                    <h1 className='titulo-login'>Registrate</h1><br />
 
-            <label className='nombre-usuario'>Ingrese su Nombre:</label>
-            <input name="nombre" className='campo-usuario' type="text"/><br />
-                            
-            <label className='nombre-usuario'>Nombre de Usuario:</label>
-            <input name='usuario' className='campo-usuario' type="text" /><br />
+                    <label className='nombre-usuario'>Ingrese su Nombre:</label>
+                    <input value = {form.nombre} onChange={this.handleChange} name="nombre" className='campo-usuario' type="text"/><br />
+                                    
+                    <label className='nombre-usuario'>Nombre de Usuario:</label>
+                    <input value = {form.usuario} onChange={this.handleChange} name='usuario' className='campo-usuario' type="text" /><br />
 
-            <label className='nombre-usuario'>Ingrese su Correo:</label>
-            <input name="correo" className='campo-usuario' type="text"/><br />
-                            
-            <label className='nombre-usuario'>Ingrese Contraseña:</label>
-            <input name="password" className='campo-usuario' type="password" /><br />
+                    <label className='nombre-usuario'>Ingrese su Correo:</label>
+                    <input value = {form.correo} onChange={this.handleChange} name="correo" className='campo-usuario' type="text"/><br />
+                                    
+                    <label className='nombre-usuario'>Ingrese Contraseña:</label>
+                    <input value = {form.password} onChange={this.handleChange} name="password" className='campo-usuario' type="password" /><br />
 
-            <center><button type="submit" className='boton-login'>Registrarme</button></center>
+                    <center><button onClick={()=>this.peticionPost()} type="submit" className='boton-login'>Registrarme</button></center>
 
-        </form>
+                </form>
+            </div>
+
+        </div>
+
     </div>
 
 </div>
-
-
-
-</div>
-
-<script src="../js"></script>
-
-    </div>;
+    )
+}
 }
 
 export default Signup
